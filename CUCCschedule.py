9b0fa4a63551cd1f572af79f164253140849b3b0
mjso7660 <somj94@gmail.com>
2018-02-16 23:50:04 -0500

    Update CUCCschedule.py

--- CUCCschedule.py
+++ CUCCschedule.py
@@ -6,9 +6,9 @@ Date: 04 Feb 2018
 class hour:
     def __init__(self, name, slot):
         '''
-            :param slot <int> number of slots
-                workers <class student> - workers assigned
-                preference <class student> - preference of students
+        :param slot <int> number of slots
+        workers <class student> - workers assigned
+        preference <class student> - preference of students
         '''
         self.slot = slot
         self.name = name
@@ -39,8 +39,11 @@ class hour:
     
     def try_matching(self, candidate):
         '''
-            :param candidate <class stdudent> student to add to an hour.
-        add the new candidate and take out least wanted candidate'''
+        add the new candidate and take out least wanted candidate
+        
+        param candidate <class stdudent> student to add to an hour.
+        return: True if candidate is matched, False othrwise
+        '''
         self._add_candidate(candidate)
         candidate.work_hour += 1
         if self._is_over():
@@ -48,8 +51,8 @@ class hour:
             dropped_worker.taken[self] = False
             dropped_worker.work_hour -= 1
             self.workers = self.workers[:-1]            
-#            return candidate == rejected_worker
-#        return True
+            return candidate != dropped_worker
+        return True
             
     def _add_candidate(self, candidate):
         ''' adds candidate to worker array'''
@@ -71,17 +74,18 @@ class hour:
                 
             
 class candidate:
-    def __init__(self, name):
+    def __init__(self, name, preferences):
         self.name = name
-        self.preferences = []
+        self.preferences = preferences
         self.work_hour = 0
         self.points = 0
-
-    @property
-    def taken(self):
+        self.taken = self.get_taken()
+        
+    def get_taken(self):
         '''initializes 'taken' according to preferences
         return: dictionary of preferences <class hour> as keys and availability <bool> as values'''
         taken = {}
+        print(self.preferences)
         for preference in self.preferences:
             taken[preference] = False
         return taken
@@ -90,6 +94,13 @@ class match:
     def __init__(self):
         self.students = []
         self.hours = []
+
+    @property
+    def count(self):
+        count = 0
+        for worker in self.hours:
+            count += len(worker.get_workers())
+        return count
     
     def add_student(self, student):
         self.students.append(student)
@@ -97,55 +108,94 @@ class match:
     def add_hour(self, hour):
         self.hours.append(hour)
     
-    def assign(self):
+    def _build_preference(self):
+        for student in self.students:
+            for hour in student.preferences:
+                hour.preferences.append(student)
+    
+    def assign_until(self):
+        self._build_preference()
+        while True:
+            count = self.count
+            self._assign()
+            print("test:", count, self.count)
+            if count == self.count:
+                return
+    
+    def _assign(self):
         '''
         assigns each candidate's [preference] to [hours]
+        return: None
         '''
         students = self.students        
-#        finished = False
-#        while not finished:
-#            finished = True
         for student in students:
             print("------",student.name,"------")
-            for preference in student.preferences:
-                print("------", preference.name,"------")
-                if not student.taken[preference]:
-                    print("*",preference.name,"preference: ")
-                    preference.preferences = self.calculate_preference(preference)
-                    preference.try_matching(student)
-#                        if not preference.try_matching(student):
-#                            finished = False
+            for hour in student.preferences:
+                print("------", hour.name,"------")
+                if not student.taken[hour]:
+                    print("*",hour.name,"preference: ")
+                    hour.preferences = self._calculate_preference(hour)
+                    print(self._slot_left(hour),self._student_with_same_point(hour, student))
+                    if self._slot_left(hour) >= self._student_with_same_point(hour, student):
+                        if hour.try_matching(student):
+                            student.taken[hour] = True
+                        
+    def _student_with_same_point(self, hour, student):
+        cnt = 0
+        try:
+            index = hour.preferences.index(student)
+        except:
+            print("error")
+            return
+        temp = index
+        while True:
+            if temp>0 and student.points == hour.preferences[temp-1].points:
+                cnt += 1
+                temp -= 1
+            else: 
+                break
+        temp = index
+        while True:
+            if temp+1 < len(hour.preferences) and student.points == hour.preferences[temp+1].points:
+                cnt += 1
+                temp += 1
+            else:
+                break
+        return cnt+1
+        
+    def _slot_left(self,hour):
+        return hour.slot - len(hour.workers)
     
-    def calculate_preference(self, hour):
+    def _sort_preference(self, hour):
+        '''
+        *sorts students based on their points
+        return: ordered preference list of students
+        '''
+        preference_list = []
+        for student in hour.preferences:
+            preference_list.append((student, student.points))
+        preference_list.sort(key=takeSecond, reverse=True)
+        for x in preference_list:
+            print(x[0].name,x[1])
+        return [i[0] for i in preference_list]
+
+    def _calculate_preference(self, hour):
         '''
         :para [hour]: <class hour> orders preferred students
         return: preference list
         '''
         for student in self.students:
             student.points = 0
-            student.points += self.calculate_hours_taken(student)
-            student.points += self.calculate_consecutive(student, hour)
+            student.points += self._calculate_hours_taken(student)
+            student.points += self._calculate_consecutive(student, hour)
 #            print(student.name, "has score", student.points)
-        return self.sort_preference()
-
-    def sort_preference(self):
-        '''
-        *sorts students based on their points
-        return: preference list of students
-        '''
-        preference_list = []
-        for student in self.students:
-            preference_list.append((student, student.points))
-        preference_list.sort(key=takeSecond, reverse=True)
-        for x in preference_list:
-            print(x[0].name,x[1])
-        return [i[0] for i in preference_list]
+        return self._sort_preference(hour)
         
-    def calculate_hours_taken(self, student):
+    def _calculate_hours_taken(self, student):
         ''' returns points based on how many hours the student has taken'''
         return 20 - student.work_hour
     
-    def calculate_consecutive(self, student, hour):
+    def _calculate_consecutive(self, student, hour):
         ''' returns points based on consecutive hours'''
         point = 0
         n_th = 1
@@ -169,18 +219,18 @@ if __name__ == '__main__':
     h2 = hour('h2', 2)   
     h3 = hour('h3', 2)
     
-    c0 = candidate('c0')    
-    c1 = candidate('c1')    
-    c2 = candidate('c2')    
-    c3 = candidate('c3')    
+    c0_pref = [h1]
+    c1_pref = [h1]
+    c2_pref = [h1, h0]
+    c3_pref = [h1, h0]
+    
+    c0 = candidate('c0', c0_pref)    
+    c1 = candidate('c1', c1_pref)    
+    c2 = candidate('c2', c2_pref)    
+    c3 = candidate('c3', c3_pref)    
     
 #    h0.preferences = [c3,c1,c2,c0]
 #    h1.preferences = [c1,c2,c0,c3]
-    
-    c0.preferences = [h0, h1]
-    c1.preferences = [h1, h0]
-    c2.preferences = [h0, h1]
-    c3.preferences = [h1, h0]
     
     students = [c0, c1, c2, c3]
     hours = [h0,h1,h2,h3]
@@ -190,10 +240,11 @@ if __name__ == '__main__':
         new_match.add_student(x)
     for y in hours:
         new_match.add_hour(y)
-    new_match.assign()
+    new_match.assign_until()
     
-#    for x in h1.workers:
-#        print(x.name)
-    
-
-    
+    print("---h0 workers----")
+    for x in h0.workers:
+        print(x.name)
+    print("---h1 workers----")
+    for x in h1.workers:
+        print(x.name)
