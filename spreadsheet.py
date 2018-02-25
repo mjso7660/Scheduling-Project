c6138ac745df8fce739a75689fd3b7b7260ff353
mjso7660 <somj94@gmail.com>
2018-02-24 21:23:40 -0500

    Update spreadsheet.py

--- spreadsheet.py
+++ spreadsheet.py
@@ -10,23 +10,46 @@ client = gspread.authorize(creds)
  
 # Find a workbook by name and open the first sheet
 # Make sure you use the right name here.
-sheet = client.open("cucc scheduling").sheet1
-all_values = sheet.get_all_values()
+sheet1 = client.open("cucc scheduling").sheet1
+sheet2 = client.open("cucc scheduling").get_worksheet(2)
+
+all_values = sheet1.get_all_values()
+all_values2 = sheet2.get_all_values()
 
 def create_hours():
+    """
+    creates hours based off from hours listed on the google spreadsheet
+    return: hours: list of hours / hour_dict: dictionary where key = hour.name, value = class Hour
+    """
     hours = []
     hour_dict = {}
-    row = 11
-    while row < len(all_values) and all_values[row][1] != '':
-        new_hour = cucc.Hour(all_values[row][2],int(all_values[row][1]))
-        hours.append(new_hour)
-        hour_dict[all_values[row][2]] = new_hour
-        row += 1
+    row,col = 2, 0
+    while col < len(all_values2[0]):
+        while row < len(all_values2) and all_values2[row][0] != 'End':
+            if all_values2[row][col+1] == '':
+                row += 1
+                continue
+            hour_name = all_values2[0][col] + all_values2[row][col]
+            print(hour_name)
+            new_hour = cucc.Hour(hour_name,int(all_values2[row][col+1]))
+            hours.append(new_hour)
+            hour_dict[hour_name] = new_hour
+            row += 1
+        empty_hour = cucc.Hour('empty',0)
+        hours.append(empty_hour)
+        #for weekends
+        if col > 25:
+            col += 4
+        else:
+            col += 6
+        row = 2
+    hour_dict['empty'] = None
     return hours, hour_dict
 
 def create_students(hour_dict):
     """
-    return: candidate list
+    creates a list of students, each an instance of class Candidate 
+    return: candidate array
     """
     student_list = []
     row = 0
@@ -41,15 +64,21 @@ def create_students(hour_dict):
         row += 1
     return student_list
 
-def update_cells(students, hours):
-    for x in range(len(hours)):
-        hour = hours[x]
-        cell_list = sheet.range(12+x, 4, 12+x, 3 + hour.slot)
-        for y in range(len(cell_list)):
-            try:
-                cell_list[y].value = hour.workers[y].name
-            except IndexError:
-                continue
-        sheet.update_cells(cell_list)
+def update_cells(hours):
+    """
+    updates cells based on students assigned from cucc.py, each day at a time
+    return: None
+    """
+    
+    cell_list = sheet2.range(3,3,17,6)
+    index = 0
+    while index < len(cell_list): 
+        for worker in hours[index//4].workers:
+            cell_list[index].value = worker.name
+            index += 1
+        while index%4 != 0:
+            index+= 1
+    sheet2.update_cells(cell_list)
         
-        
+if __name__ == '__main__':
+    pass
